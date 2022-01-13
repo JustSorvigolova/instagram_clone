5affe015b9097be93179327738da701a770e104a
Rustanov <akbarrustanov@gmail.com>
2022-01-13 20:00:37 +0600

    create a database,sign up,firebase

--- src/component/Post.js
+++ src/component/Post.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './Post.css'
 import {Avatar} from "@mui/material";
 
-const Post = ({userName, imageUrl, caption}) => {
+const Post = ({username, imageUrl, caption}) => {
     return (
         <div className="post">
             <div className="div post_header">
                 <Avatar className="post_avatar" alt="Remy Sharp" src="https://bigpicture.ru/wp-content/uploads/2015/11/nophotoshop29-800x532.jpg"/>
 
-                <h3>{userName}</h3>
+                <h3>{username}</h3>
              </div>
             <img alt="lol" className="post_image" src={imageUrl}/>
-            <h4 className="post_text"><strong>{userName}</strong> {caption} </h4>
+            <h4 className="post_text"><strong>{username}</strong> {caption} </h4>
 
         </div>
     );
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
-import {initializeApp} from "firebase/app";
 import firebase from "firebase/compat";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCAeWnsr4KU_OSovuG8glb_rJl9agkJOT8",
-    authDomain: "instagram-clone-react-64a18.firebaseapp.com",
-    projectId: "instagram-clone-react-64a18",
-    storageBucket: "instagram-clone-react-64a18.appspot.com",
-    messagingSenderId: "454479612029",
-    appId: "1:454479612029:web:e0fff227aa4e814a2f4914"
-};
-export default firebaseConfig;
+
+
 const firebaseApp= firebase.initializeApp({
     apiKey: "AIzaSyCAeWnsr4KU_OSovuG8glb_rJl9agkJOT8",
     authDomain: "instagram-clone-react-64a18.firebaseapp.com",
@@ -18,6 +10,7 @@ const firebaseApp= firebase.initializeApp({
     messagingSenderId: "454479612029",
     appId: "1:454479612029:web:e0fff227aa4e814a2f4914"
 })
+export default firebaseApp;
 
 const db = firebaseApp.firestore()
 const auth = firebaseApp.auth()
